162851d19cf36931c203449b1b0da6d08557949d
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-31 17:05:51 +0200

    The Add / Edit Fragment

---
+++ app/src/main/java/com/octavian/tasktimer/MainActivityFragment.kt
@@ -0,0 +1,18 @@
+package com.octavian.tasktimer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class MainActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
