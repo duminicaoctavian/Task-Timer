238b0842eead7da099659389bf7e7da57e1d77a6
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-31 15:27:38 +0200

    Main Menu UI

--- app/src/main/java/com/octavian/tasktimer/MainActivity.kt
+++ app/src/main/java/com/octavian/tasktimer/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.octavian.tasktimer
 
-import android.content.ContentResolver
 import android.content.ContentValues
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
@@ -20,11 +17,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        testInsert()
-        testUpdate()
-        testUpdateTwo()
+//        testInsert()
+//        testUpdate()
+//        testUpdateTwo()
 //        testDelete()
-        testDeleteTwo()
+//        testDeleteTwo()
 
 //        val appDatabase = AppDatabase.getInstance(this)
 //        val db = appDatabase.readableDatabase
@@ -116,7 +113,7 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.menumain_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
