1a4fcbce6533362fcb8e94ac5277c6b2af1a7531
Octavian Duminica <duminicaoctavian@gmail.com>
2020-11-01 15:09:12 +0200

    Compatibility Challenge Solution Part 2

--- app/src/main/java/com/octavian/tasktimer/MainActivity.kt
+++ app/src/main/java/com/octavian/tasktimer/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.octavian.tasktimer
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +11,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.content_main.*
@@ -128,6 +132,19 @@ class MainActivity : AppCompatActivity(),
 
         val aboutVersion = messageView.findViewById(R.id.about_version) as TextView
         aboutVersion.text = BuildConfig.VERSION_NAME
+
+        // Use a nullable type: the TextView won't exist on API 21 and higher
+        val aboutUrl: TextView? = messageView.findViewById(R.id.about_url)
+        aboutUrl?.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            val s = (it as TextView).text.toString()
+            intent.data = Uri.parse(s)
+            try {
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(this@MainActivity, R.string.about_url_error, Toast.LENGTH_LONG).show()
+            }
+        }
 
         aboutDialog?.show()
     }
