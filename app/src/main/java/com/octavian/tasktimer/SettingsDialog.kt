c360f245c405779f7721a5768cb084113b9a7826
Octavian Duminica <duminicaoctavian@gmail.com>
2020-11-06 21:00:11 +0200

    DefaultSharedPreferences

--- app/src/main/java/com/octavian/tasktimer/SettingsDialog.kt
+++ app/src/main/java/com/octavian/tasktimer/SettingsDialog.kt
@@ -1,16 +1,26 @@
 package com.octavian.tasktimer
 
 import android.os.Bundle
+import android.preference.PreferenceManager.getDefaultSharedPreferences
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatDialogFragment
 import kotlinx.android.synthetic.main.settings_dialog.*
+import java.util.*
 
 private const val TAG = "SettingsDialog"
 
+const val SETTINGS_FIRST_DAY_OF_WEEK = "FirstDay"
+const val SETTINGS_IGNORE_LESS_THAN = "IgnoreLessThan"
+const val SETTINGS_DEFAULT_IGNORE_LESS_THAN = 0
+
 class SettingsDialog: AppCompatDialogFragment() {
+
+    private val defaultFirstDayOfWeek = GregorianCalendar(Locale.getDefault()).firstDayOfWeek
+    private var firstDay = defaultFirstDayOfWeek
+    private var ignoreLessThan = SETTINGS_DEFAULT_IGNORE_LESS_THAN
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         Log.d(TAG, "onCreateView: called")
@@ -22,12 +32,48 @@ class SettingsDialog: AppCompatDialogFragment() {
 
         super.onViewCreated(view, savedInstanceState)
         okButton.setOnClickListener {
-            // saveValues()
+            saveValues()
             dismiss()
         }
 
         cancelButton.setOnClickListener {
             dismiss()
+        }
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        Log.d(TAG, "inViewStateRestored: called")
+
+        super.onViewStateRestored(savedInstanceState)
+        readValues()
+
+        firstDaySpinner.setSelection(firstDay - GregorianCalendar.SUNDAY)
+
+        ignoreSeconds.progress = ignoreLessThan
+    }
+
+    private fun readValues() {
+        with(getDefaultSharedPreferences(context)) {
+            firstDay = getInt(SETTINGS_FIRST_DAY_OF_WEEK, defaultFirstDayOfWeek)
+            ignoreLessThan = getInt(SETTINGS_IGNORE_LESS_THAN, SETTINGS_DEFAULT_IGNORE_LESS_THAN)
+        }
+        Log.d(TAG, "Retrieving first day = $firstDay, ignoreLessThan = $ignoreLessThan")
+    }
+
+    private fun saveValues() {
+        val newFirstDay = firstDaySpinner.selectedItemPosition + GregorianCalendar.SUNDAY
+        val newIgnoreLessThan = ignoreSeconds.progress
+
+        Log.d(TAG, "Saving first day = $newFirstDay, ignore seconds: $newIgnoreLessThan")
+
+        with(getDefaultSharedPreferences(context).edit()) {
+            if (newFirstDay != firstDay) {
+                putInt(SETTINGS_FIRST_DAY_OF_WEEK, newFirstDay)
+            }
+            if (newIgnoreLessThan != ignoreLessThan) {
+                putInt(SETTINGS_IGNORE_LESS_THAN, newIgnoreLessThan)
+            }
+            apply()
         }
     }
 }
