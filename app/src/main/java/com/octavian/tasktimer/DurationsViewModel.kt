6f8d040760cd04d53108896078057fd684e4e718
Octavian Duminica <duminicaoctavian@gmail.com>
2020-11-07 18:05:43 +0200

    Filtering in the DurationsViewModel

--- app/src/main/java/com/octavian/tasktimer/DurationsViewModel.kt
+++ app/src/main/java/com/octavian/tasktimer/DurationsViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.util.*
 
 private const val TAG = "DurationsViewModel"
 
@@ -19,6 +20,8 @@ enum class SortColumns {
 }
 
 class DurationsViewModel(application: Application): AndroidViewModel(application) {
+
+    private val calendar = GregorianCalendar()
 
     private val databaseCursor = MutableLiveData<Cursor>()
     val cursor: LiveData<Cursor>
@@ -33,19 +36,86 @@ class DurationsViewModel(application: Application): AndroidViewModel(application
         }
 
     private val selection = "${DurationsContract.Columns.START_TIME} Between ? AND ?"
-    private var selectionArgs = arrayOf("1556668800", "1559347199")
+    private var selectionArgs = emptyArray<String>()
 
     private var _displayWeek = true
     val displayWeek: Boolean
         get() = _displayWeek
 
     init {
-        loadData()
+        applyFilter()
     }
 
     fun toggleDisplayWeek() {
         _displayWeek = !_displayWeek
-        // applyFilter()
+        applyFilter()
+    }
+
+    fun getFilterDate(): Date {
+        return calendar.time
+    }
+
+    fun setReportDate(year: Int, month: Int, dayOfMonth: Int) {
+        // check if the date has changed
+        if (calendar.get(GregorianCalendar.YEAR) != year
+            || calendar.get(GregorianCalendar.MONTH) != month
+            || calendar.get(GregorianCalendar.DAY_OF_MONTH) != dayOfMonth) {
+
+            calendar.set(year, month, dayOfMonth, 0, 0, 0)
+            applyFilter()
+        }
+    }
+
+    private fun applyFilter() {
+        Log.d(TAG, "Entering applyFilter")
+
+        val currentCalendarDate = calendar.timeInMillis // store the time, so we can put it back
+
+        if (displayWeek) {
+            // show records for the entire week
+
+            // we have a date, so find out which day of the week it is
+            val weekStart = calendar.firstDayOfWeek
+            Log.d(TAG, "applyFilter: first day of calendar week is $weekStart")
+            Log.d(TAG, "applyFilter: dayOfWeek is ${calendar.get(GregorianCalendar.DAY_OF_WEEK)}")
+            Log.d(TAG, "applyFilter: date is " + calendar.time)
+
+            // calculate week start and end dates
+            calendar.set(GregorianCalendar.DAY_OF_WEEK, weekStart)
+            calendar.set(GregorianCalendar.HOUR_OF_DAY, 0) // Note: HOUR_OF_DAY, not HOUR
+            calendar.set(GregorianCalendar.MINUTE, 0)
+            calendar.set(GregorianCalendar.SECOND, 0)
+            val startDate = calendar.timeInMillis / 1000
+
+            // move forward 6 days to get to the last day of the week
+            calendar.add(GregorianCalendar.DATE, 6)
+            calendar.set(GregorianCalendar.HOUR_OF_DAY, 23)
+            calendar.set(GregorianCalendar.MINUTE, 59)
+            calendar.set(GregorianCalendar.SECOND, 59)
+            val endDate = calendar.timeInMillis / 1000
+
+            selectionArgs = arrayOf(startDate.toString(), endDate.toString())
+            Log.d(TAG, "In applyFilter(7), Start Date is $startDate, End Date is $endDate")
+        } else {
+            // re-query for the current day
+            calendar.set(GregorianCalendar.HOUR_OF_DAY, 0)
+            calendar.set(GregorianCalendar.MINUTE, 0)
+            calendar.set(GregorianCalendar.SECOND, 0)
+            val startDate = calendar.timeInMillis / 1000
+
+            calendar.set(GregorianCalendar.HOUR_OF_DAY, 23)
+            calendar.set(GregorianCalendar.MINUTE, 59)
+            calendar.set(GregorianCalendar.SECOND, 59)
+            val endDate = calendar.timeInMillis / 1000
+
+            selectionArgs = arrayOf(startDate.toString(), endDate.toString())
+            Log.d(TAG, "In applyFilter(1), Start Date is $startDate, End Date is $endDate")
+        }
+
+        // put the calendar back to where it was before we started jumping back and forth
+        calendar.timeInMillis = currentCalendarDate
+
+        loadData()
     }
 
     private fun loadData() {
